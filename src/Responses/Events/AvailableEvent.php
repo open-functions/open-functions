97d10e0af7039bc7c0fa01def7afeae0709cf9ff
Florian Weigang <florian.g.weigang@gmail.com>
2025-04-10 15:59:55 +0200

    update

--- src/Responses/Events/AvailableEvent.php
+++ src/Responses/Events/AvailableEvent.php
@@ -7,4 +7,18 @@ class AvailableEvent
     public string $namespace;
     public string $name;
     public string $identifier;
+
+    /**
+     * Constructor to initialize an AvailableEvent object.
+     *
+     * @param string $namespace  The namespace of the event.
+     * @param string $name       The name of the event.
+     * @param string $identifier The unique identifier for the event.
+     */
+    public function __construct(string $namespace, string $name, string $identifier)
+    {
+        $this->namespace = $namespace;
+        $this->name = $name;
+        $this->identifier = $identifier;
+    }
 }
